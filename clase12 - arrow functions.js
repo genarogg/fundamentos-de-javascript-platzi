6dae2805184e30976b2b060bbf9312923bcc339d
genarogg <gena_gonzalez15@hotmail.com>
2019-10-01 02:07:49 -0400

    clase12 - arrow functions.js p1

---
+++ clase12 - arrow functions.js
@@ -0,0 +1,17 @@
+var sacha = {
+    nombre: 'Sacha',
+    apellido: 'Lifszyc',
+    edad: 28,
+    ingeniero: false,
+    cocinero: false,
+    cantante: true,
+    dj: false,
+    guitarrista: false,
+    drone: false
+  }
+  
+  var juan = {
+    nombre: 'Juan',
+    apellido: 'Gomez',
+    edad: 13
+  }
