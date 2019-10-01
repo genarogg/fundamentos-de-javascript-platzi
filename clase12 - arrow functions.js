31a440282ae0899b53f43c7463ae5322c3ceb4e2
genarogg <gena_gonzalez15@hotmail.com>
2019-10-01 02:09:13 -0400

    clase12 - arrow functions.js p3

--- clase12 - arrow functions.js
+++ clase12 - arrow functions.js
@@ -45,3 +45,17 @@ function imprimirProfesiones(persona) {
       console.log('Piloto de drone')
     }
   }
+  
+const MAYORIA_DE_EDAD = 18
+
+function esMayorDeEdad(persona) {
+  return persona.edad >= MAYORIA_DE_EDAD
+}
+
+function imprimirSiEsMayorDeEdad(persona) {
+  if (esMayorDeEdad(persona)) {
+    console.log(`${persona.nombre} es mayor de edad`)
+  } else {
+    console.log(`${persona.nombre} es menor de edad`)
+  }
+}
