4cc14510bb6e0f685b7823572dd049c97ab5ddc8
genarogg <gena_gonzalez15@hotmail.com>
2019-10-06 14:56:59 -0400

    clase31 - manejo de errores con callbacks.js p1

---
+++ clase31 - manejo de errores con callbacks.js
@@ -0,0 +1,13 @@
+const API_URL = 'https://swapi.co/api/'
+const PEOPLE_URL = 'people/:id'
+const opts = { crossDomain: true }
+
+function obtenerPersonaje(id, callback) {
+  const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+
+  $
+    .get(url, opts, callback)
+    .fail(() => {
+      console.log(`Sucedió un error. No se pudo obtener el personaje ${id}`)
+    })
+}
