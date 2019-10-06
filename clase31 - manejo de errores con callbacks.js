13a3b9f8938c88a28340aead4a5524c25e1591dd
genarogg <gena_gonzalez15@hotmail.com>
2019-10-06 14:57:26 -0400

    clase31 - manejo de errores con callbacks.js p2

--- clase31 - manejo de errores con callbacks.js
+++ clase31 - manejo de errores con callbacks.js
@@ -11,3 +11,31 @@ function obtenerPersonaje(id, callback) {
       console.log(`Sucedió un error. No se pudo obtener el personaje ${id}`)
     })
 }
+
+obtenerPersonaje(1, function (personaje) {
+    console.log(`Hola, yo soy ${personaje.name}`)
+  
+    obtenerPersonaje(2, function (personaje) {
+      console.log(`Hola, yo soy ${personaje.name}`)
+  
+      obtenerPersonaje(3, function (personaje) {
+        console.log(`Hola, yo soy ${personaje.name}`)
+  
+        obtenerPersonaje(4, function (personaje) {
+          console.log(`Hola, yo soy ${personaje.name}`)
+  
+          obtenerPersonaje(5, function (personaje) {
+            console.log(`Hola, yo soy ${personaje.name}`)
+  
+            obtenerPersonaje(6, function (personaje) {
+              console.log(`Hola, yo soy ${personaje.name}`)
+  
+              obtenerPersonaje(7, function (personaje) {
+                console.log(`Hola, yo soy ${personaje.name}`)
+              })
+            })
+          })
+        })
+      })
+    })
+  })
