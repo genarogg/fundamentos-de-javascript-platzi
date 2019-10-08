4163fc434a51752a5cb23f46936613bf3660713b
genarogg <gena_gonzalez15@hotmail.com>
2019-10-08 15:07:14 -0400

    clase35 - async-await.js

--- clase35 - async-await.js
+++ clase35 - async-await.js
@@ -9,3 +9,23 @@ function obtenerPersonaje(id) {
       .get(url, opts, function (data) {
         resolve(data)
       })
+      .fail(() => reject(id))
+  })
+}
+
+function onError(id) {
+  console.log(`Sucedió un error al obtener el personaje ${id}`)
+}
+
+async function obtenerPersonajes() {
+  var ids = [1, 2, 3, 4, 5, 6, 7]
+  var promesas = ids.map(id => obtenerPersonaje(id))
+  try {
+    var personajes = await Promise.all(promesas)
+    console.log(personajes)
+  } catch (id) {
+    onError(id)
+  }
+}
+
+obtenerPersonajes()
