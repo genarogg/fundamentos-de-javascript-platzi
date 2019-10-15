53daeec6e6fada0bea1aa8a2386385b119b91f68
genarogg <gena_gonzalez15@hotmail.com>
2019-10-14 23:10:42 -0400

    formateado de codigo

--- 11-20/clase15 - loops do-while.js
+++ 11-20/clase15 - loops do-while.js
@@ -1,13 +1,13 @@
-var contador = 0
+const contador = 0;
 
-const llueve = () => Math.random() < 0.25
+const llueve = () => Math.random() < 0.25;
 
 do {
-  contador++
-} while (!llueve())
+  contador++;
+} while (!llueve());
 
 if (contador === 1) {
-  console.log(`Fui a ver si llovía ${contador} vez`)
+  console.log(`Fui a ver si llovía ${contador} vez`);
 } else {
-  console.log(`Fui a ver si llovía ${contador} veces`)
+  console.log(`Fui a ver si llovía ${contador} veces`);
 }
