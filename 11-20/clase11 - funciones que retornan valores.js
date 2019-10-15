1f4447a18138f135acff00a357e813e50eb37cd9
genarogg <gena_gonzalez15@hotmail.com>
2019-10-14 23:07:29 -0400

    formateado de codigo

--- 11-20/clase11 - funciones que retornan valores.js
+++ 11-20/clase11 - funciones que retornan valores.js
@@ -1,62 +1,61 @@
-var sacha = {
-    nombre: 'Sacha',
-    apellido: 'Lifszyc',
-    edad: 28,
-    ingeniero: false,
-    cocinero: false,
-    cantante: true,
-    dj: false,
-    guitarrista: false,
-    drone: false
+const sacha = {
+  nombre: "Sacha",
+  apellido: "Lifszyc",
+  edad: 28,
+  ingeniero: false,
+  cocinero: false,
+  cantante: true,
+  dj: false,
+  guitarrista: false,
+  drone: false
+};
+
+const juan = {
+  nombre: "Juan",
+  apellido: "Gomez",
+  edad: 13
+};
+
+function imprimirProfesiones(persona) {
+  console.log(`${persona.nombre} es:`);
+
+  if (persona.ingeniero) {
+    console.log("Ingeniero");
+  } else {
+    console.log("No es ingeniero");
   }
-  
-  var juan = {
-    nombre: 'Juan',
-    apellido: 'Gomez',
-    edad: 13
+
+  if (persona.cocinero) {
+    console.log("Cocinero");
   }
-  
-  function imprimirProfesiones(persona) {
-    console.log(`${persona.nombre} es:`)
-  
-    if (persona.ingeniero) {
-      console.log('Ingeniero')
-    } else {
-      console.log('No es ingeniero')
-    }
-  
-    if (persona.cocinero) {
-      console.log('Cocinero')
-    }
-  
-    if (persona.dj) {
-      console.log('DJ')
-    }
-  
-    if (persona.cantante) {
-      console.log('Cantante')
-    }
-  
-    if (persona.guitarrista) {
-      console.log('Gutiarrista')
-    }
-  
-    if (persona.drone) {
-      console.log('Piloto de drone')
-    }
+
+  if (persona.dj) {
+    console.log("DJ");
   }
-  
-  const MAYORIA_DE_EDAD = 18
-  
-  function esMayorDeEdad(persona) {
-    return persona.edad >= MAYORIA_DE_EDAD
+
+  if (persona.cantante) {
+    console.log("Cantante");
   }
-  
-  function imprimirSiEsMayorDeEdad(persona) {
-    if (esMayorDeEdad(persona)) {
-      console.log(`${persona.nombre} es mayor de edad`)
-    } else {
-      console.log(`${persona.nombre} es menor de edad`)
-    }
+
+  if (persona.guitarrista) {
+    console.log("Gutiarrista");
   }
-  
+
+  if (persona.drone) {
+    console.log("Piloto de drone");
+  }
+}
+
+const MAYORIA_DE_EDAD = 18;
+
+function esMayorDeEdad(persona) {
+  return persona.edad >= MAYORIA_DE_EDAD;
+}
+
+function imprimirSiEsMayorDeEdad(persona) {
+  if (esMayorDeEdad(persona)) {
+    console.log(`${persona.nombre} es mayor de edad`);
+  } else {
+    console.log(`${persona.nombre} es menor de edad`);
+  }
+}
