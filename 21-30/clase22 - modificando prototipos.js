4086c6b5b34516c7fae04febf0abde450717f03e
genarogg <gena_gonzalez15@hotmail.com>
2019-10-15 22:21:52 -0400

    formateado de codigo

--- 21-30/clase22 - modificando prototipos.js
+++ 21-30/clase22 - modificando prototipos.js
@@ -1,19 +1,19 @@
 function Persona(nombre, apellido, altura) {
-    this.nombre = nombre
-    this.apellido = apellido
-    this.altura = altura
-  }
-  
-  Persona.prototype.saludar = () => {
-    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`)
-  }
-  
-  Persona.prototype.soyAlto = () => this.altura > 1.8
-  
-  var sacha = new Persona('Sacha', 'Lifszyc', 1.72)
-  var erika = new Persona('Erika', 'Luna', 1.65)
-  var arturo = new Persona('Arturo', 'Martinez', 1.89)
-  
-  sacha.soyAlto()
-  erika.soyAlto()
-  arturo.soyAlto()
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.altura = altura;
+}
+
+Persona.prototype.saludar = () => {
+  console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
+};
+
+Persona.prototype.soyAlto = () => this.altura > 1.8;
+
+const sacha = new Persona("Sacha", "Lifszyc", 1.72);
+const erika = new Persona("Erika", "Luna", 1.65);
+const arturo = new Persona("Arturo", "Martinez", 1.89);
+
+sacha.soyAlto();
+erika.soyAlto();
+arturo.soyAlto();
