e44ea467a2df26738c67b6a995ba522a21925b2f
genarogg <gena_gonzalez15@hotmail.com>
2019-10-15 22:28:28 -0400

    formateado de codigo

--- 31-41/clase34 - multiples promesas en paralelo.js
+++ 31-41/clase34 - multiples promesas en paralelo.js
@@ -1,33 +1,30 @@
-const API_URL = 'https://swapi.co/api/'
-const PEOPLE_URL = 'people/:id'
-const opts = { crossDomain: true }
+const API_URL = "https://swapi.co/api/";
+const PEOPLE_URL = "people/:id";
+const opts = { crossDomain: true };
 
 function obtenerPersonaje(id) {
   return new Promise((resolve, reject) => {
-    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-    $
-      .get(url, opts, function (data) {
-        resolve(data)
-      })
-      .fail(() => reject(id))
-  })
+    const url = `${API_URL}${PEOPLE_URL.replace(":id", id)}`;
+    $.get(url, opts, function(data) {
+      resolve(data);
+    }).fail(() => reject(id));
+  });
 }
 
 function onError(id) {
-    console.log(`Sucedió un error al obtener el personaje ${id}`)
-  }
-  
-  var ids = [1, 2, 3, 4, 5, 6, 7]
-  // var promesas = ids.map(function (id) {
-  //   return obtenerPersonaje(id)
-  // })
-  var promesas = ids.map(id => obtenerPersonaje(id))
-  Promise
-    .all(promesas)
-    .then(personajes => console.log(personajes))
-    .catch(onError)
+  console.log(`Sucedió un error al obtener el personaje ${id}`);
+}
 
-    // obtenerPersonaje(1)
+var ids = [1, 2, 3, 4, 5, 6, 7];
+// var promesas = ids.map(function (id) {
+//   return obtenerPersonaje(id)
+// })
+var promesas = ids.map(id => obtenerPersonaje(id));
+Promise.all(promesas)
+  .then(personajes => console.log(personajes))
+  .catch(onError);
+
+// obtenerPersonaje(1)
 //   .then(personaje1 => {
 //     console.log(`El personaje 1 es ${personaje1.name}`)
 //     return obtenerPersonaje(2)
