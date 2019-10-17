bc4ccf677ea50061c112603ce03a66750e7b2a18
genarogg <gena_gonzalez15@hotmail.com>
2019-10-17 12:05:16 -0400

    constructor del juego

--- proyecto del curso/js/main.js
+++ proyecto del curso/js/main.js
@@ -5,3 +5,38 @@ const naranja = document.getElementById('naranja');
 const verde = document.getElementById('verde');
 const btnEmpezar = document.getElementById('btnEmpezar');
 const ULTIMO_NIVEL = 3;
+
+class Juego {
+
+    constructor() {
+
+        this.inicializar = this.inicializar.bind(this);
+        this.inicializar();
+        this.generarSecuencia();
+        setTimeout(this.siguienteNivel, 500)
+    }
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+    
+    
+
+    
+
+    
+
+    
+
+    
+}
